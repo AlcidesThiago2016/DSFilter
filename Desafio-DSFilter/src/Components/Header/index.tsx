b0c02f78b0727086145e18d279b9f03c2fa4e583
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-03-11 20:16:31 -0300

    create products and product-service

---
+++ Desafio-DSFilter/src/Components/Header/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function Header(){
+    return(
+        <header className="dsf-mb20">
+            <div className="dsf-container header">
+                <h1>DSFilter</h1>
+            </div>
+        </header>
+    );
+}
