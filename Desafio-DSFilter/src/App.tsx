ab445661bf0ef2c3a1544525466d5d747e6d3915
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-03-11 22:01:53 -0300

    update app.tsx

--- Desafio-DSFilter/src/App.tsx
+++ Desafio-DSFilter/src/App.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import Header from "./Components/Header";
+import { ContextProductCount } from "./utils/context-product";
+import ListingBody from "./Components/ListingBody";
 
 function App() {
+  const [contextProductCount, setContextProductCount] = useState<number>(0);
 
   return (
-    <Header />
-  )
+    <>
+    <ContextProductCount.Provider value={{contextProductCount,setContextProductCount}}>
+      <Header />
+      <ListingBody />
+    </ContextProductCount.Provider>
+    </>
+  );
 }
 
-export default App
+export default App;
